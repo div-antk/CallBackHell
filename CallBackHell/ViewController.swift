acc56ed6b7734b4c2a352a3094978156b1952190
div-antk <51283468+owaii@users.noreply.github.com>
2021-07-13 22:27:56 +0900

    コールバック書いてみる

--- CallBackHell/ViewController.swift
+++ CallBackHell/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        // kがコールバック
+        let sum = { (x: Int, y: Int, k: (Int) -> Int) in
+            return k(x + y)
+        }
 
+//        var num = sum(1, sum(2, 3))
+
+        label.text = String(num)
+    }
 
 }
 
