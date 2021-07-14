bc6483df2b1c30c6658f60c1cba330613bb050ea
div-antk <51283468+owaii@users.noreply.github.com>
2021-07-14 23:27:23 +0900

    型推論を学ぶ

--- CallBackHell/ViewController.swift
+++ CallBackHell/ViewController.swift
@@ -14,14 +14,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // kがコールバック
-        let sum = { (x: Int, y: Int, k: (Int) -> Int) in
-            return k(x + y)
+        // 引数を省略するクロージャのパターン
+        let division = { $0 * 40 }
+        print(division(20))
+        
+        let checker = { price in
+            if price > 1000 {
+                print("高価")
+            } else {
+                print("そうでもない")
+            }
         }
-
-//        var num = sum(1, sum(2, 3))
-
-        label.text = String(num)
+        
+        
+        //年齢から、成人済みかのメッセージを返す
+        let ageCheckMessage = { age in
+            if age < 20 {
+                print("ha")
+            }
+        }
+         
+        //クロージャを実行する
+        print(ageCheckMessage(26))
+        
+        print(checker(200))
+//        // kがコールバック
+//        let sum = { (x: Int, y: Int, k: (Int) -> Int) in
+//            return k(x + y)
+//        }
+//
+////        var num = sum(1, sum(2, 3))
+//
+//        label.text = String(num)
     }
 
 }
